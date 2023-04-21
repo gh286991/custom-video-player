c7477413d01f42428c26e118566e045dcbb1c9f6
gh286991 <gh286991@gmail.com>
2023-04-22 00:21:12 +0800

    feat:對話視窗

--- src/Components/Canvas/Hooks/useDrawCanvas.ts
+++ src/Components/Canvas/Hooks/useDrawCanvas.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { IBarrage } from '../Modules/Barrages';
 
 import { drawBarrages, drawAdvertisement, drawMarquee } from '../Modules';
@@ -37,6 +37,29 @@ const useDrawCanvas = ({
   ctx,
 }: UseDrawProps) => {
   const marqueeXRef = useRef(0); 
+  const [isBoxVisible, setIsBoxVisible] = useState(true); // 添加状态
+
+  const drawBoxWithText = () => {
+    if (!canvas || !video || !container) return;
+    if (!ctx) return;
+    const boxWidth = 200;
+    const boxHeight = 100;
+    const x = (canvas.width - boxWidth) / 2;
+    const y = (canvas.height - boxHeight) / 2;
+  
+    // 绘制背景
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+    // 绘制方框
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fillRect(x, y, boxWidth, boxHeight);
+  
+    // 绘制文本
+    ctx.fillStyle = '#000000';
+    ctx.font = '16px Arial';
+    ctx.fillText('ccvcvcvvc', x + 10, y + 30);
+  };
 
   useEffect(() => {
     if (!canvas || !video || !container) return;
@@ -52,6 +75,10 @@ const useDrawCanvas = ({
       drawAdvertisement(ctx, adText, adWidth, adHeight);
       drawBarrages(ctx, barrages);
 
+      if (isBoxVisible) {
+        drawBoxWithText(); // 调用绘制方框的函数
+      }
+
       requestID = requestAnimationFrame(draw);
     };
 
@@ -60,22 +87,19 @@ const useDrawCanvas = ({
     return () => {
       cancelAnimationFrame(requestID);
     };
-  }, [
-    barrages,
-    canvas,
-    video,
-    container,
-    text,
-    speed,
-    fontSize,
-    fontColor,
-    backgroundColor,
-    adText,
-    adWidth,
-    adHeight,
-    isExpanded,
-    ctx,
-  ]);
+  }, [barrages, canvas, video,
+    container, text, speed, fontSize, fontColor,
+    backgroundColor, adText, adWidth, adHeight, isExpanded, isBoxVisible, ctx, drawBoxWithText]);
+
+  const toggleBoxVisibility = () => {
+    setIsBoxVisible(!isBoxVisible);
+  };
+
+  return {
+    toggleBoxVisibility,
+    isBoxVisible,
+  };
+  
 };
 
 export default useDrawCanvas;
--- src/Components/Canvas/index.tsx
+++ src/Components/Canvas/index.tsx
@@ -36,7 +36,7 @@ const CanvasContainer: React.FC<IMarqueeCanvasProps> = ({
   const [inputValue, setInputValue] = useState<string>('');
   const { barrages, addBarrage } = useAddBarrage({ canvasRef,
     speed: 2 });
-  
+
   const ctx = canvasRef.current?.getContext('2d');
   const canvas = canvasRef.current;
   const video = videoRef.current;
@@ -44,7 +44,7 @@ const CanvasContainer: React.FC<IMarqueeCanvasProps> = ({
 
   useUpdateCanvasSize(canvas, video, container, isExpanded);
 
-  useDrawCanvas({
+  const { toggleBoxVisibility, isBoxVisible } = useDrawCanvas({
     barrages,
     canvas,
     video,
@@ -70,6 +70,20 @@ const CanvasContainer: React.FC<IMarqueeCanvasProps> = ({
           left: '0px',
           pointerEvents: 'none' }}
       />
+      {isBoxVisible && (
+        <button
+          onClick={toggleBoxVisibility}
+          style={{
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 10, // 确保按钮位于 Canvas 之上
+          }}
+        >
+          确定
+        </button>
+      )}
       <input
         style={{ position: 'absolute',
           zIndex: 2,
